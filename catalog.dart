f6687374d3924c28cddf323c370aabd0fdbfa39b
Marco Dal Corso <marco6dc@gmail.com>
2022-06-28 22:57:14 +0200

    nuovo file catalog.dart

---
+++ catalog.dart
@@ -0,0 +1,5 @@
+//This class acts as data repository for the product catalog of the store.
+class Catalog {
+  //For simplicity, a product is represented as a String.
+  List<String> products = ['Ham', 'Cheese', 'Wine', 'Pasta'];
+} //Catalog
